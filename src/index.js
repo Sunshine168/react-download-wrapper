3e625ea793c6c65417fe5ec29e292ac0bd8b3b27
mai <168496714@qq.com>
2018-03-27 18:08:46 +0800

    init project

---
+++ src/index.js
@@ -0,0 +1,70 @@
+// @flow
+import * as React from 'react'
+
+export type Param = {
+  name: string,
+  value: any,
+}
+
+export type PropsType = {
+  onClick(e: SyntheticEvent): void,
+  method: string,
+  action: string,
+  params: Array<Param>,
+  children: React.Children,
+  renderInputItem(param: Param): React$Element<*>,
+}
+
+export type DefaultProps = {
+  params: Array<Param>,
+}
+
+export default class DownloadWrapper extends React.Component<DefaultProps, PropsType, *> {
+  static defaultProps: DefaultProps = {
+    params: [],
+  }
+  onClick = (e: SyntheticEvent): void => {
+    const { form } = this
+    if (!(form instanceof window.HTMLElement)) {
+      return
+    }
+
+    form.submit()
+    const { onClick } = this.props
+    if (typeof onClick === 'function') {
+      onClick(e)
+    }
+  }
+
+  form: HTMLElement
+
+  render() {
+    const {
+      method, children, action, params, renderInputItem, ...otherProps
+    } = this.props
+    const elem = React.Children.only(children)
+    const Element = React.cloneElement(elem, {
+      ...otherProps,
+      onClick: this.onClick,
+    })
+    return (
+      <form method={method} action={action} ref={form => (this.form = form)}>
+        {Element}
+        {params.map((param) => {
+          if (renderInputItem) {
+            return renderInputItem(param)
+          }
+          return (
+            <input
+              name={param.name}
+              value={param.value}
+              hidden
+              key={param.name}
+              onChange={() => {}}
+            />
+          )
+        })}
+      </form>
+    )
+  }
+}
