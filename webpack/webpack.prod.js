720a41d8fac73537a61a0bb146d4b3bdb0ca5a1b
mai <168496714@qq.com>
2018-03-28 09:52:53 +0800

    Removed useless line

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,8 +1,6 @@
 const path = require('path')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
-const HotMiddleWareConfig = 'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000'
-
 module.exports = {
   entry: path.resolve(__dirname, '../src/index.js'),
   output: {
